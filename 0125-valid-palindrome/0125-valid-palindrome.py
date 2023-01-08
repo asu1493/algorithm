74f63b9c83295edb5bd8282a413c4a470c7c6285
asu1493 <qnfpdhrwka1@gmail.com>
2023-01-08 23:20:20 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (10.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,23 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = re.sub('[^a-z0-9]', '', s)
-        return s == s[::-1]
+bool isPalindrome(char * s){
+    int bias_left = 0;
+    int bias_right = 1;
+    
+    int str_len = strlen(s);
+    for( int i = 0; i < str_len; i++){
+        while(!isalnum(s[i + bias_left])){
+            bias_left++;
+            if(i + bias_left == str_len)
+                return true;
+        }
+        while(!isalnum(s[str_len - i - bias_right])){
+            bias_right++;
+        }
+
+        if(i+bias_left >= str_len -i - bias_right)
+            break;
+
+        if( tolower(s[ i + bias_left]) != tolower(s[str_len - i - bias_right]))
+            return false;
+    }
+    return true;
+}
